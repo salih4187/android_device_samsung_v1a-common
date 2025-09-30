retiredtab/android_device_samsung_klimt-common

klimt: Exclude overlay/hardware/samsung/AdvancedDisplay from RRO
Change-Id: Id612d051188859586c0ca15c692ef7b9ffcb1003

device-common.mk
@@ -21,7 +21,8 @@ DEVICE_PATH := device/samsung/klimt-common
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    device/samsung/klimt-common/overlay/lineage-sdk
+    device/samsung/klimt-common/overlay/lineage-sdk \
+    device/samsung/klimt-common/overlay/hardware/samsung/AdvancedDisplay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := xlarge

salih4187/android_device_samsung_v1a-common

device-common.mk
@@ -20,6 +20,8 @@ DEVICE_PATH := device/samsung/v1a-common
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    device/samsung/v1a-common/overlay/hardware/samsung/AdvancedDisplay
 
 # AdvancedDisplay (MDNIE)
 PRODUCT_PACKAGES += \